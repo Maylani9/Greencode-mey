8625c356ad563d3c5da451837274af1cdd31af53
meylani9 <meylani1214@gmail.com>
2023-10-02 17:49:16 +0700

    addOperator

---
+++ Basic-Programing/operasi.js
@@ -0,0 +1,15 @@
+let angka1 = 12;
+let angka2 = 7;
+let perkalian = 12 * 7;
+
+let firstName = "mey";
+let lastName = "lani";
+let fullName = firstName + " " + lastName;
+
+let angka4 = 7;
+let angka5 = 4;
+let pembagian = angka4 / angka5;
+let pertambahan = angka4 + angka5;
+let pengurangan = angka4 - angka5;
+let sisaBagi = angka4 % angka5;
+console.log(sisaBagi);
---
+++ Basic-Programing/variable.js
@@ -0,0 +1,11 @@
+var myname = "meymey";
+let myAge = "20";
+const myAddres = "Siantar";
+let isMahasiswa = true;
+let myBOY;
+let laniLana = null;
+console.log(laniLana);
+
+console.log(myBOY);
+
+console.log(isMahasiswa);
