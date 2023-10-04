43e5cca8d89933c08a237a9cd48c13e3d8d706d4
meylani9 <meylani1214@gmail.com>
2023-10-04 15:09:35 +0700

    add Perbandingan

---
+++ Basic-Programing/operasi/increment.js
@@ -0,0 +1,10 @@
+let z = 20;
+//ini perkalian 3
+z *= 3;
+z += 3;
+z -= 3;
+z /= 3;
+z %= 3;
+console.log(z);
+let bolls = true + false;
+console.log(bolls);
---
+++ Basic-Programing/operasi/perbandingan.js
@@ -0,0 +1,10 @@
+//ini perbandingan operasi lebih besar dan lebih kecil
+let perbandingan = 5 > 2;
+let lebihBesar = 6 > 3;
+let lebihKecil = 6 < 3;
+let besarSamaDengan = 6 >= 3;
+let kecilSamaDengan = 6 <= 3;
+let samaDengan = 6 == 3;
+let tidakSamaDengan = 6 != 3;
+let tidakSamaDengan2x = 6 !== 3;
+let samaDengan3 = "6" === 6;
