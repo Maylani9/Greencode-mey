e72f85ba48552f044a24c913fa5ddb52d8dd0cc4
meylani9 <meylani1214@gmail.com>
2023-10-04 14:24:34 +0700

    add Operasi Aritmatika

---
+++ Basic-Programing/operasi/aritmatika.js
@@ -0,0 +1,13 @@
+let x = 10;
+let y = 5;
+let pertambahan = x + y;
+let pengurangan = x - y;
+let perkalian = x * y;
+let pembagian = x / y;
+let sisaBagi = x % y;
+
+console.log("x + y = ", pertambahan);
+console.log("x - y = ", pengurangan);
+console.log("x * y = ", perkalian);
+console.log("x / y = ", pembagian);
+console.log("x % y = ", sisaBagi);
