b36968a5074a4f80a974c55af6b53761af6ce867
meylani9 <meylani1214@gmail.com>
2023-10-09 16:42:58 +0700

    add perkondisian

---
+++ Basic-Programing/perkondisian/perkondisian.js
@@ -0,0 +1,25 @@
+let mahasiswa = false;
+if (mahasiswa == true) {
+  console.log("aku mahasiswa");
+} else {
+  console.log("aku bukan mahasiswa");
+}
+let nilai = 1;
+let isMahasiswa = false;
+if (isMahasiswa == true) {
+  nilai++;
+} else {
+}
+nilai--;
+console.log(nilai);
+
+let angka = 75;
+let hasil = "nilaiku";
+if (angka > 90) {
+  hasil = " nilai sangat bagus";
+} else if (angka < 90) {
+  hasil = "nilai cukup bagus";
+} else if (angka < 70) {
+  nilai = "nilai bagus";
+}
+console.log(hasil);
