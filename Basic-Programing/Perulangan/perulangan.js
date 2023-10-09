7da8d7f1e63cd268539b3f8b70265a7bebfa0e7d
meylani9 <meylani1214@gmail.com>
2023-10-09 17:36:56 +0700

    add perulangan

---
+++ Basic-Programing/Perulangan/perulangan.js
@@ -0,0 +1,9 @@
+for (let index = 1; index <= 100; index += 2) {
+  console.log(index);
+}
+
+let fruits = ["mangga", "rambutan", "jambu"];
+
+for (let index = 0; index < 3; index++) {
+  console.log(fruits[index]);
+}
