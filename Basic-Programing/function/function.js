58a71d13c8dd3db1f7c8ec149749d6d582a436f8
meylani9 <meylani1214@gmail.com>
2023-10-16 14:58:22 +0700

    add function

--- Basic-Programing/function/function.js
+++ Basic-Programing/function/function.js
@@ -19,3 +19,21 @@ function kalkulasi(a, b, fungsi) {
   return fungsi(a, b);
 }
 console.log(kalkulasi(3, 5, pengurangan));
+
+function Pembagian(a, b) {
+  return a / b;
+}
+//let VarPembagian = Pembagian(10, 2);
+console.log(Pembagian(10, 2));
+
+function angka(angkaAwal, angkaAkhir) {
+  for (let index = 1; index <= 20; index++) {
+    console.log(index);
+  }
+}
+angka(1, 20);
+
+function pangkat2(angka) {
+  return angka * angka;
+}
+console.log(pangkat2(8));
